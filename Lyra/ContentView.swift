71e675e6e467530dd21899b91c6356004b721bf7
Joan Wilson <joanwmoliveira@gmail.com>
2023-04-03 09:33:50 -0300

    chore: project creation

---
+++ Lyra/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Lyra
+//
+//  Created by Joan Wilson Oliveira on 03/04/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Lyra/LyraApp.swift
@@ -0,0 +1,17 @@
+//
+//  LyraApp.swift
+//  Lyra
+//
+//  Created by Joan Wilson Oliveira on 03/04/23.
+//
+
+import SwiftUI
+
+@main
+struct LyraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
