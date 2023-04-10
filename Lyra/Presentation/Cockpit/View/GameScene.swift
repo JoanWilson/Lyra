f275f284b1642bf8670b18f814e90e05bc55416a
Luiz Sena <senaluiz1307@gmail.com>
2023-04-10 11:18:14 -0300

    add: place a uiview over skscene insde a viewcontroller

---
+++ Lyra/Presentation/Cockpit/View/GameScene.swift
@@ -0,0 +1,17 @@
+//
+//  GameScene.swift
+//  Lyra
+//
+//  Created by Luiz Sena on 10/04/23.
+//
+
+import UIKit
+import SpriteKit
+
+class GameScene: SKScene {
+
+    override func didMove(to view: SKView) {
+        view.backgroundColor = .purple
+    }
+
+}
--- Lyra/Presentation/Cockpit/ViewController/CockpitViewController.swift
+++ Lyra/Presentation/Cockpit/ViewController/CockpitViewController.swift
@@ -6,12 +6,40 @@
 //
 
 import UIKit
+import SpriteKit
 
 class CockpitViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
+
+    }
+
+    override func viewDidLayoutSubviews() {
+        let skView = SKView(frame: view.frame)
+        skView.backgroundColor = .green
+        self.view.addSubview(skView)
+        if let view = skView as SKView? {
+            let scene = GameScene(size: view.bounds.size)
+//            let scene = GameScene(size: CGSize(width: 2388, height: 1668))
+            scene.scaleMode = .aspectFill
+            scene.backgroundColor = .systemPurple
+
+            view.presentScene(scene)
+
+            view.ignoresSiblingOrder = false
+            view.showsPhysics = false
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+
+        let subView = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        subView.backgroundColor = .blue
+        self.view.addSubview(subView)
+
+
+
+
     }
     
 
