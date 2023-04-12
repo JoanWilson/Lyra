69568f069f8ccb63272a855c32008a686c1665c4
Joan Wilson <joanwmoliveira@gmail.com>
2023-04-12 11:51:39 -0300

    add: add new methods to StorageProtocol to make it more testable

--- Lyra/Data/Protocols/StorageProtocol.swift
+++ Lyra/Data/Protocols/StorageProtocol.swift
@@ -8,10 +8,12 @@
 import Foundation
 
 public protocol StorageProtocol {
-    func createGameState(with gameState: GameStateEntity) -> Bool
+    func createGameState(with gameState: GameStateEntity?) -> GameStateEntity?
     func removeGameState(with gameState: GameStateEntity) -> Bool
-    func updateGameState(with gameState: GameStateEntity) -> GameStateEntity
-    func addRune(with rune: RuneEntity) -> Bool
+    func updateGameState(with gameState: GameStateEntity) -> GameStateEntity?
+    func addRune(with rune: RuneEntity) -> RuneEntity?
     func removeRune(with rune: RuneEntity) -> Bool
-    func updateRune(with rune: RuneEntity) -> RuneEntity
+    func updateRune(with rune: RuneEntity) -> RuneEntity?
+    func compareGameStates(gameState: GameState?, gameStateEntity: GameStateEntity?) -> GameStateEntity?
+    func compareRunes() -> RuneEntity?
 }
